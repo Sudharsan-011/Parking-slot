6a56f61c0d690b27415caa8c97ef474c73b97bb5
Viswa <viswabala1603@gmail.com>
2025-11-11 22:21:20 +0530

    Added Parking Module

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 // ===== Start Server =====
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
